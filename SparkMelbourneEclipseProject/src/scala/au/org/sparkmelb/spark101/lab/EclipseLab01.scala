cc80d0c3833664be633efe91f097e7dd02b26b25
Timothy Findlay <tfindlay@prodevelop.com.au>
2015-04-18 22:35:06 +1000

    Draft of Eclipse project. Some definitions to be auto-generated by Gradle

---
+++ SparkMelbourneEclipseProject/src/scala/au/org/sparkmelb/spark101/lab/EclipseLab01.scala
@@ -0,0 +1,29 @@
+package scala.au.org.sparkmelb.spark101.lab
+
+// Import libraries
+import org.apache.spark.sql.SQLContext
+import org.apache.spark.SparkConf
+import org.apache.spark.SparkContext
+
+/**
+ * @author timothy.findlay
+ */
+object EclipseLab01 {
+
+  def main(args: Array[String]) {
+
+    // Set Configuration
+  
+    // Get Context
+    
+    // Setup SQL Context to get Dataframe
+  
+    // Define Filename, store in a static variable fileName
+
+    // Perform the read
+     
+    // Display results
+
+  }
+
+}
---
+++ SparkMelbourneEclipseProject/src/scala/au/org/sparkmelb/spark101/solution/EclipseLab01.scala
@@ -0,0 +1,35 @@
+package scala.au.org.sparkmelb.spark101.solution
+
+// Import libraries
+import org.apache.spark.sql.SQLContext
+import org.apache.spark.SparkConf
+import org.apache.spark.SparkContext
+
+/**
+ * @author timothy.findlay
+ */
+object EclipseLab01 {
+
+  def main(args: Array[String]) {
+
+    // Set Configuration
+    val sparkConf = new SparkConf().setAppName("EclipseLab01").setMaster("local[1]")
+  
+    // Get Context
+    val sparkContext = new SparkContext( sparkConf )
+    
+    // Setup SQL Context to get Dataframe
+    val sqlContext = new SQLContext( sparkContext )  
+  
+    // Define Filename, store in a static variable fileName
+    val fileName="C:\\SparkMelbourne\\spark-1.3.1-bin-hadoop2.6\\data\\people.json"
+
+    // Perform the read
+    var newRows = sqlContext.jsonFile( fileName )
+     
+    // Display results
+    println( "Read " +  newRows.count() + " row(s) from: " + fileName)  
+
+  }
+
+}
