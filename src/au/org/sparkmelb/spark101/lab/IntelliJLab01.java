e00fb2f8fb488b39ab45a2c9a0f07a3af7dedfe1
tfindlay-au <tfindlay@prodevelop.com.au>
2015-04-18 20:57:52 -0400

    Added Java examples

---
+++ src/au/org/sparkmelb/spark101/lab/IntelliJLab01.java
@@ -0,0 +1,32 @@
+package java.au.org.sparkmelb.spark101.lab;
+
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.api.java.function.FlatMapFunction;
+import java.util.Arrays;
+
+/**
+ * Example program in Java to demonstrate
+ * - Use of textFile to read raw text =
+ * - Use of JavaRDD
+ * - Use of FlatMap
+ *
+ */
+public class IntelliJLab01 {
+
+    public void main(String[] argsc) {
+
+        // Setup Spark Configuration
+
+        // Define Java Spark Context
+
+        // Perform the read from file creating an RDD
+
+        // Lets do something different, do a word count
+
+        // Display results
+
+    }
+}
+
---
+++ src/au/org/sparkmelb/spark101/solution/IntelliJLab01.java
@@ -0,0 +1,46 @@
+package java.au.org.sparkmelb.spark101.solution;
+
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.api.java.function.FlatMapFunction;
+import java.util.Arrays;
+
+/**
+ * Example program in Java to demonstrate
+ * - Use of textFile to read raw text =
+ * - Use of JavaRDD
+ * - Use of FlatMap
+ *
+ */
+public class IntelliJLab01 {
+
+    public void main(String[] argsc) {
+
+        // Setup Spark Configuration
+        SparkConf sparkConf = new SparkConf().setAppName("IntelliJLab01").setMaster("local[1]");
+
+        // Define Java Spark Context
+        JavaSparkContext ctx = new JavaSparkContext(sparkConf);
+
+        // Perform the read. Filename specified in-line
+        JavaRDD<String> lines = ctx.textFile("./data/people.json", 1);
+
+        // Lets do something different, do a word count
+        JavaRDD<String> words = lines.flatMap(
+                new FlatMapFunction<String, String>() {
+                    @Override
+                    public Iterable<String> call(String s) {
+                        return Arrays.asList(s.split(" "));
+                    }
+                }
+        );
+
+        // Display results
+        System.out.println("Read " + lines.count() + " row(s) with " + words.count() + " words" );
+
+        ctx.stop();
+
+    }
+}
+
